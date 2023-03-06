da926ae3179a3b1be3f09880b41f5b106895040e
Z1z1J11 <126528068+Z1z1J11@users.noreply.github.com>
2023-03-06 08:05:35 +0800

    Add files via upload

---
+++ c3.6.c
@@ -0,0 +1,74 @@
+#include <stdio.h>  
+#include <string.h>
+#include<math.h>
+#define  _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_GLOBALS
+#pragma warning(disable:4996)	
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc = arr;
+//	printf("%s\n", arr);
+//	printf("%s\n", pc);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc = &arr;
+//	printf("%s\n", arr);
+//	printf("%p\n", pc);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc =arr;
+//    printf("%s\n", arr);
+//	printf("%s\n", pc);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc =&arr;
+//
+//	printf("%s\n", arr);
+//	printf("%s\n", pc);
+//	printf("%p\n", arr);
+//	return 0;
+////}
+//int main()
+//{
+//	char* p = "abcdef";
+//	printf("%c\n", *p);
+//	printf("%s\n", p);
+//	return 0;
+//int main()
+//{
+//	char* p = "abcdef";
+//	
+//	printf("%s\n", p);
+//	return 0;  //segmentfault
+////
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//	char* p1 = "abcdef";
+//	char *p2 = "abcdef";
+//	if (p1== p2)
+//	{
+//		printf("hhh\n");
+//	}
+//	else {
+//		printf("haha");
+//	}
+//	/*if (arr1 == arr2)
+//	{
+//		printf("hhh\n");
+//	}
+//	else {
+//		printf("haha");
+//	}	*/					  return 0;	
+//}

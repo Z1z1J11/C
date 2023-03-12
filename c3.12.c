f5e21628651c8063bd16618799201cce97bf7701
Z1z1J11 <126528068+Z1z1J11@users.noreply.github.com>
2023-03-12 10:58:42 +0800

    Add files via upload

---
+++ c3.12.c
@@ -0,0 +1,71 @@
+#define	_CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+#include<assert.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//extern int Add(int x, int y);
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+//#define MAX 100
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);  //
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	FILE* pf = fopen("log.txt", "w");
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		fprintf(pf, "file:%s line:%d date:%s time:%s", __FILE__, __LINE__, __DATE__, __TIME__,i);
+//		printf("%d ", arr[i]);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n",__STDC__);
+//	return 0;
+//}

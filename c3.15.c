742e81cfa8d4867d0016dcc87e0bc900f8814c52
Z1z1J11 <126528068+Z1z1J11@users.noreply.github.com>
2023-03-15 11:16:55 +0800

    Add files via upload

---
+++ c3.15.c
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#define	_CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <assert.h>
+using namespace std
+//void get_next(SString &T,int &next[])
+//{
+//	int i = 1;
+//	int next[1] = 0;
+//	int j = 0;
+//	while (i < T[0])
+//	{
+//		if (j = 0 || T[i] == T[j])
+//		{
+//			++i;
+//			++j;
+//			next[i] = j;
+//			if ()
+//		}
+//		else
+//		{
+//			j = next[j];
+//		}
+//	}
+//}	   kmp算法
+//void get_nextval(SString& T, int& nextval[])
+//{
+//	int i = 1;
+//	int nextval[1] = 0;
+//	int j = 0;
+//	while (i < T[0])
+//	{
+//		if (j = 0 || T[i] == T[j])
+//		{
+//			++i;
+//			++j;
+//			if (T[i] != T[j])
+//			{
+//				nextval[i] = j;
+//			}
+//			else
+//			{
+//				nextval[i] = nextval[j];
+//			}
+//		}
+//		else j =  nextval[j];		
+//	}
+//}	//kmp算法优化
